c7f9d6cc7360503f450869e7bb2ac271fb813802
vitemelly44 <iankibiwot108@gmail.com>
2026-06-14 09:44:12 -0700

    Publish static site to gh-pages

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import viteReact from "@vitejs/plugin-react";
-
-export default defineConfig({
-  base: "./",
-  vite: {
-    plugins: [viteReact()],
-  },
-});
